f5ed8dfe934ff33a62adc6abd6147ee80c100fee
Anton <43760202+Anton-219@users.noreply.github.com>
2026-09-23 21:54:21 +0200

    Fix popup operator namespace after addon rename

--- modules/logging.py
+++ modules/logging.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def log(message, show_popup = False, icon="INFO"):
 
-    print(f"[SpriteSheetMaker {datetime.now()}] {message}")
+    print(f"[Simplified SpriteSheetMaker {datetime.now()}] {message}")
 
     if(show_popup):
-        bpy.ops.spritesheetmaker.message_popup('INVOKE_DEFAULT', **{ "message_heading": message,  "message_icon" : icon })
+        bpy.ops.simplified_spritesheetmaker.message_popup('INVOKE_DEFAULT', **{ "message_heading": message,  "message_icon" : icon })
